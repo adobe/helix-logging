758db0478e4b3190401f79ef1ff9aaf2c66610ff
Lars Trieloff <trieloff@adobe.com>
2019-01-03 11:26:39 +0000

    better credential passing, check for existence of service account

--- index.js
+++ index.js
@@ -1,20 +1,57 @@
-const {google} = require('googleapis')
+const googleapis = require("googleapis");
+const path = require("path");
+const request = require("request-promise-native");
+const key = require("./service-account-key.json");
 
-async function main () {
-
-  // This method looks for the GCLOUD_PROJECT and GOOGLE_APPLICATION_CREDENTIALS
-  // environment variables.
-  const auth = await google.auth.getClient({
+async function main() {
+  const auth = await googleapis.google.auth.getClient({
     // Scopes can be specified either as an array or as a single, space-delimited string.
-    scopes: ['https://www.googleapis.com/auth/compute']
+    scopes: ["https://www.googleapis.com/auth/cloud-platform"],
+    credentials: {
+      client_email: key.client_email,
+      private_key: key.private_key
+    }
   });
 
-  // obtain the current project Id
-  const project = await google.auth.getProjectId();
+  const headers = await googleapis.google.auth.getRequestHeaders(
+    "https://iam.googleapis.com/v1/projects/helix-225321/serviceAccounts"
+  );
 
-  // Fetch the list of GCE zones within a project.
-  const res = await compute.zones.list({ project, auth });
-  console.log(res.data);
+  try {
+    const response = await request.post(
+      "https://iam.googleapis.com/v1/projects/helix-225321/serviceAccounts",
+      {
+        headers,
+        json: true,
+        body: {
+          accountId: "foo-bar",
+          serviceAccount: {
+            displayName: "foo-bar Logging Account"
+          }
+        }
+      }
+    );
+
+    console.log(response);
+  } catch (e) {
+    // service account already exists
+
+    const account = await request.get(
+      await googleapis.google.auth.authorizeRequest({
+        uri:
+          "https://iam.googleapis.com/v1/projects/helix-225321/serviceAccounts/foo-bar@helix-225321.iam.gserviceaccount.com",
+        json: true,
+      })
+    );
+
+    console.log(account);
+  }
+
+  const { accounts } = await request.get(
+    "https://iam.googleapis.com/v1/projects/helix-225321/serviceAccounts",
+    { headers, json: true }
+  );
+  console.log(accounts.map(account => account.name));
 }
 
 main().catch(console.error);
