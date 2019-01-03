5063ab0c408bde4d3a1a9fbbe5464a214bda2f20
Lars Trieloff <trieloff@adobe.com>
2019-01-03 12:45:43 +0000

    enforce full code coverage

---
+++ src/google/auth.js
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2018 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+const { google } = require('googleapis');
+
+/**
+ *
+ * @param {String} email the email of the service account to use for all subsequent requests
+ * @param {String} key the private key of the service account to use for all subsequent requests
+ */
+async function auth(email, key) {
+  try {
+    google.auth.cachedCredential = null;
+
+    const client = await google.auth.getClient({
+      // Scopes can be specified either as an array or as a single, space-delimited string.
+      scopes: ['https://www.googleapis.com/auth/cloud-platform'],
+      credentials: {
+        client_email: email,
+        private_key: key,
+      },
+    });
+
+    // verifying that the credentials are valid
+    await google.auth.getRequestHeaders(
+      'https://iam.googleapis.com/v1/',
+    );
+
+    return client;
+  } catch (e) {
+    throw new Error(`Invalid credentials. Make sure email is the Google Service 
+Account email in the format <name>@<project>.iam.gserviceaccount.com and key is
+the private key starting with -----BEGIN PRIVATE KEY-----. You can create a new
+Service Account in the Google Cloud Platform Console under IAM.`);
+  }
+}
+
+module.exports = {
+  auth,
+};
