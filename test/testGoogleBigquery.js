e3b6277836f3c7b984e6662d58c0eddff40c5af2
Lars Trieloff <trieloff@adobe.com>
2019-01-04 14:44:03 +0000

    update table schema

--- test/testGoogleBigquery.js
+++ test/testGoogleBigquery.js
@@ -41,7 +41,44 @@ describe('Test google.bigquery', () => {
     });
 
     it('Testing successful table creation', async () => {
-      const dataset = await createTable(process.env.CLIENT_EMAIL, process.env.PRIVATE_KEY.replace(/\\n/g, '\n'), process.env.PROJECT_ID, 'test_dataset', 'test_logs');
+      const dataset = await createTable(process.env.CLIENT_EMAIL, process.env.PRIVATE_KEY.replace(/\\n/g, '\n'), process.env.PROJECT_ID, 'test_dataset', 'test_logs', [
+        { name: 'client_as_name', type: 'string' },
+        { name: 'client_geo_city', type: 'string' },
+        { name: 'client_geo_conn_speed', type: 'string' },
+        { name: 'client_geo_continent_code', type: 'string' },
+        { name: 'client_geo_country_code', type: 'string' },
+        { name: 'client_geo_gmt_offset', type: 'string' },
+        { name: 'client_geo_latitude', type: 'string' },
+        { name: 'client_geo_longitude', type: 'string' },
+        { name: 'client_geo_metro_code', type: 'string' },
+        { name: 'client_geo_postal_code', type: 'string' },
+        { name: 'client_geo_region', type: 'string' },
+        { name: 'client_ip_hashed', type: 'string' },
+        { name: 'client_ip_masked', type: 'string' },
+        { name: 'resp_http_x_openwhisk_activation_id', type: 'string' },
+        { name: 'fastly_info_state', type: 'string' },
+        { name: 'req_http_host', type: 'string' },
+        { name: 'req_http_Referer', type: 'string' },
+        { name: 'req_http_User_Agent', type: 'string' },
+        { name: 'req_http_X_CDN_Request_ID', type: 'string' },
+        { name: 'req_http_X_Host', type: 'string' },
+        { name: 'req_http_X_Owner', type: 'string' },
+        { name: 'req_http_X_Ref', type: 'string' },
+        { name: 'req_http_X_Repo', type: 'string' },
+        { name: 'req_http_X_Static', type: 'string' },
+        { name: 'req_http_X_Strain', type: 'string' },
+        { name: 'req_http_X_URL', type: 'string' },
+        { name: 'req_url', type: 'string' },
+        { name: 'resp_http_Content_Type', type: 'string' },
+        { name: 'resp_http_X_Version', type: 'string' },
+        { name: 'server_datacenter', type: 'string' },
+        { name: 'server_region', type: 'string' },
+        { name: 'service_config', type: 'string' },
+        { name: 'status_code', type: 'string' },
+        { name: 'time_end_usec', type: 'string' },
+        { name: 'time_start_usec', type: 'string' },
+        { name: 'vcl_sub', type: 'string' },
+      ]);
       assert.ok(dataset);
     });
 
