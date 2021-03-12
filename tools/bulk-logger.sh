ab84ac7e9af289a39a891f5ddfe9f2a33dd2a4ad
Lars Trieloff <trieloff@adobe.com>
2021-03-12 11:54:07 +0100

    fix(bulk-logger): fix bash syntax
    json in bash is hard

--- tools/bulk-logger.sh
+++ tools/bulk-logger.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-fastly service list | grep .live | while read line ; do 
+fastly service list | grep skyline | while read line ; do 
   LINE=$(echo $line | grep -o -e " [0-9A-Za-z]* vcl [0-9]* ")
   ID=$(echo $LINE | sed -e "s/ .*//")
   VERSION=$(echo $LINE | sed -e "s/.* //")
   NEWVERSION=$(fastly service-version clone -s $ID --version $VERSION | sed -e "s/.* to version //")
   echo $ID "cloned" $VERSION "=>" $NEWVERSION
-  echo curl -X "POST" "https://helix-pages.anywhere.run/helix-services/logging@v1" \
-        -H 'Content-Type: application/json; charset=utf-8' \
-        -d $'{
-      "service": "'$ID'",
-      "coralogixapp": "fastly",
-      "coralogixkey": "'$CORALOGIX_TOKEN'",
-      "splunkhost": "https://splunk-hec-ext.adobe.net/services/collector",
-      "version": '$NEWVERSION',
-      "token": "'$FASTLY_AUTH'",
-      "splunkauth": "'$SPLUNK_AUTH'"
-    }'
+  curl -X "POST" "https://helix-pages.anywhere.run/helix-services/logging@v1" \
+     -H 'Content-Type: application/x-www-form-urlencoded; charset=utf-8' \
+     --data-urlencode "service=$ID" \
+     --data-urlencode "version=$NEWVERSION" \
+     --data-urlencode "token=$FASTLY_AUTH" \
+     --data-urlencode "coralogixapp=fastly" \
+     --data-urlencode "coralogixkey=$CORALOGIX_TOKEN" \
+     --data-urlencode "splunkhost=https://splunk-hec-ext.adobe.net/services/collector" \
+     --data-urlencode "splunkauth=$SPLUNK_AUTH"
+  echo ""
   fastly service-version activate -s $ID --version $NEWVERSION
 done
