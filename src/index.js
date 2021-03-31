d46e7ba7a99812769fc39f067796d621c4af1417
Lars Trieloff <trieloff@adobe.com>
2021-03-31 16:21:10 +0200

    fix(index): remove one status check

--- src/index.js
+++ src/index.js
@@ -75,7 +75,7 @@ async function setupLogger(request, context) {
  */
 module.exports.main = wrap(setupLogger)
   .with(status, {
-    fastly: 'https://api.fastly.com/public-ip-list',
+    // fastly: 'https://api.fastly.com/public-ip-list',
     googleiam: 'https://iam.googleapis.com/$discovery/rest?version=v1',
     googlebigquery: 'https://www.googleapis.com/discovery/v1/apis/bigquery/v2/rest',
   })
