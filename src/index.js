9c0d3c5db11c11e47b85888f85c6caa2bec7b761
Tobias Bocanegra <tripod@adobe.com>
2021-05-03 11:18:51 +0900

    fix(deps): use helix shared modules

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@
  */
 
 const { wrap: status } = require('@adobe/helix-status');
-const { wrap } = require('@adobe/helix-shared');
+const wrap = require('@adobe/helix-shared-wrap');
 const { logger } = require('@adobe/helix-universal-logger');
 const { Response } = require('@adobe/helix-universal');
 
