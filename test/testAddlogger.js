408965e1f5ec7b10278f97345558296d665a2961
Dominique Pfister <dpfister@adobe.com>
2021-10-20 14:12:20 +0200

    fix: add missing files

--- test/testAddlogger.js
+++ test/testAddlogger.js
@@ -14,8 +14,6 @@ const assert = require('assert');
 const { condit } = require('@adobe/helix-testutils');
 const addlogger = require('../src/addlogger');
 
-require('dotenv').config();
-
 const CI_ENVVAR_NAMES = [
   'GOOGLE_CLIENT_EMAIL',
   'GOOGLE_PRIVATE_KEY',
